fe88fa809483d87f5b4ffb55c28f26773d09419a
STEVE IAN M. <98774874+ianmakes@users.noreply.github.com>
2025-01-16 00:34:45 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: true, // This ensures the server listens on all network interfaces
+    port: 8080,
+    strictPort: true, // This ensures the server only uses the specified port
+    hmr: {
+      clientPort: 443 // This ensures proper HMR over HTTPS
+    }
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
